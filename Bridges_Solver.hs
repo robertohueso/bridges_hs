00e467de450e24c74bd1c41a446b670a0c0c3e8a
robertohueso <robertohueso96@gmail.com>
2018-01-22 17:21:42 +0100

    Add basic structure Bridge_Solver

--- Bridges_Solver.hs
+++ Bridges_Solver.hs
@@ -1,2 +1,22 @@
 import Bridges
+import Graph
 
+type Estado = Graph Tag Int
+
+estado1 = game1
+estado2 = game2
+estado3 = game3
+
+esEstadoFinal :: Estado -> Bool
+esEstadoFinal e = undefined
+
+data Movimiento = Arriba (Vertex Tag Int)
+  | Abajo (Vertex Tag Int)
+  | Izquierda (Vertex Tag Int)
+  | Derecha (Vertex Tag Int)
+
+aplicables :: Estado -> [Movimiento]
+aplicables e = undefined
+
+aplica :: Movimiento -> Estado -> Estado
+aplica m e = undefined
