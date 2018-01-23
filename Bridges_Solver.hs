140d63acc5a7c44cb3f8e11711e0b7bb2605de1d
robertohueso <robertohueso96@gmail.com>
2018-01-23 01:01:03 +0100

    Implement esEstadoFinal

--- Bridges_Solver.hs
+++ Bridges_Solver.hs
@@ -1,14 +1,17 @@
 import Bridges
 import Graph
 
-type Estado = Graph Tag Int
+type Estado = (Graph Tag Int, Vertex Tag Int)
 
-estado1 = game1
-estado2 = game2
-estado3 = game3
+estado1 = (game1, undefined)
+estado2 = (game2, undefined)
+estado3 = (game3, undefined)
 
 esEstadoFinal :: Estado -> Bool
-esEstadoFinal e = undefined
+esEstadoFinal (g, _) = foldr
+  (\(V t _) acc -> if t == 0 then True && acc else False)
+  True
+  (vertices g)
 
 data Movimiento = Arriba (Vertex Tag Int)
   | Abajo (Vertex Tag Int)
