bef5fd494be9ae62ffd5973d929a2ea129022e43
robertohueso <robertohueso96@gmail.com>
2018-01-23 17:12:18 +0100

    Adds comments to Graph

--- Graph.hs
+++ Graph.hs
@@ -9,18 +9,19 @@ module Graph
    add_vertex,
    add_edge) where
 
--- Directed edges from one vertex to another are represente by a tuple
--- (origin_vertex, destination_vertex)
 data Vertex tag coord = V tag (coord, coord)
+-- Vertex a == vertex b if their coordinates are the same.
 instance (Eq c, Num c, Num t) => Eq (Vertex t c) where
   V a (x,y) == V b (z,w) = x==z && y==w
+  a /= b = not (a==b)
 instance (Show c, Show t) => Show (Vertex t c) where
   show (V t c) = show c
 
+-- Directed edges from one vertex to another are represente by a tuple
+-- (origin_vertex, destination_vertex)
 type Edge t c = (Vertex t c, Vertex t c)
 
--- Graphs can be empty, have a single vertex or multiple vertices
--- all with some coordiantes and a tag.
+-- Graphs can be empty or have vertices
 data Graph t c = Empty | G (Vertex t c) [(Vertex t c)] (Graph t c)
   deriving Show
 
@@ -33,7 +34,7 @@ vertices :: Graph t c -> [Vertex t c]
 vertices Empty = []
 vertices (G v _ g) = [v] ++ (vertices g)
 
--- List of edges
+-- List of graph's edges
 edges :: Graph t c -> [Edge t c]
 edges (G a vs g) = [(a, b) | b <- vs] ++ (edges g)
 edges _ = []
@@ -46,9 +47,9 @@ valid_edge ((V _ a), (V _ b)) = a /= b
 add_vertex :: (Eq c) => Vertex t c -> Graph t c -> Graph t c
 add_vertex vertex g = G vertex [] g
 
+-- Add an edge to the graph
 add_edge :: (Eq c, Num c, Num t) => Edge t c -> Graph t c -> Graph t c
 add_edge _ Empty = Empty
-
 add_edge (origin,dest) (G (V t v) vs g)
   | (V t v) == origin && valid_edge (origin, dest) && not (dest `elem` vs)=
     G (V (t-1) v) (dest:vs) (add_edge (origin,dest) g)
